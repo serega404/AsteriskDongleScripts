791b401d6e148cb045a239687d05ff16baf0ee2b
Sergey Karmanov <ppoocpel8888@gmail.com>
2023-08-31 12:16:09 +0300

    Улучшил форматирование

--- sms2tg.py
+++ sms2tg.py
@@ -5,19 +5,20 @@ import sys, requests
 bot_token = "123456789:ABCdefghIJKlmnOPQrstuVWXYz"
 chat_id = "123456789"
 
-message = "Пришла новая SMS:\nОтправитель: " +  sys.argv[1] + "\nТекст: " + sys.argv[2]
+log = "SMS: От: " +  sys.argv[1] + " Текст: " + sys.argv[2]
 
-print(message)
+print(log)
 
 # Save message to log
 
 text_file = open("/var/log/sms", "a+")
-text_file.write(message)
+text_file.write(log)
 text_file.close()
 
 # Send message
 
-req = requests.get("https://api.telegram.org/bot" + bot_token + "/sendMessage?chat_id=" + chat_id + "&text=" + message)
+message = "Пришла SMS:\n\nОт: <b>" +  sys.argv[1] + "</b>\nТекст: <code>" + sys.argv[2] + "</code>"
+req = requests.get("https://api.telegram.org/bot" + bot_token + "/sendMessage?parse_mode=HTML&chat_id=" + chat_id + "&text=" + message)
 if (req.status_code != 200):
     print("Сообщение не отправлено! Статус код: " + str(req.status_code))
 else:
